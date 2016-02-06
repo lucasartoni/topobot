955a0fe8bd6de188d77c9eb9e7d3e1ad0ae222d8
Luca Sartoni <luca.sartoni@automattic.com>
2016-02-06 23:04:36 +0100

    adds support for hue lights and first working prototype

--- bin/bot.js
+++ bin/bot.js
@@ -2,10 +2,16 @@
 
 var TopoBot = require('../lib/topobot');
 
-var token = process.env.BOT_API_KEY;
+var token = process.env.BOT_API_KEY || require( '../settings/token' ),
+	username = process.env.USERNAME || require( '../settings/username'),
+	bridgeip = process.env.BRIDGEIP || require( '../settings/bridgeip');
+
+
 
 var topobot = new TopoBot({
     token: token,
+    username: username,
+    bridgeip: bridgeip,
     name: 'TopoBot'
 });
 
