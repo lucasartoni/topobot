ab7da0e10037709a5d31fb8eb5e04ffd1fa31c5b
Luca Sartoni <luca.sartoni@automattic.com>
2016-02-07 11:45:30 +0100

    better settings configuration

---
+++ settings/settings-sample.js
@@ -0,0 +1,6 @@
+{
+	"token"	: "your slack token",
+	"bridgeip" : "your hue bridge IP on the LAN",
+	"username" : "your hue uesername on the bridge",
+	"sonosip" : "your sonos device IP on the LAN" 
+}
