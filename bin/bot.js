5935e56c64b2cbcecedfdb57107a32dee0a6f3c2
Luca Sartoni <luca.sartoni@automattic.com>
2016-02-07 15:28:32 +0100

    fixed path issue on Linux

--- bin/bot.js
+++ bin/bot.js
@@ -3,7 +3,7 @@
 var TopoBot = require( '../lib/topobot' ),
 	path = require ( 'path' ),
 	fs = require ( 'fs' ),
-	configFile = path.join( __dirname + '../settings/settings.json'),
+	configFile = path.join( __dirname , '../settings/settings.json' ),
 	config = JSON.parse( fs.readFileSync( configFile ) );
 
 var topobot = new TopoBot( {
